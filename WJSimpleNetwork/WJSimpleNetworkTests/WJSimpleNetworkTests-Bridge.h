d830270d1af251ca459b6e0c51658a1ad99d54a5
Kevin <Kevin.1988@foxmail.com>
2015-03-23 00:51:13 +0800

    初始化项目

---
+++ WJSimpleNetwork/WJSimpleNetworkTests/WJSimpleNetworkTests-Bridge.h
@@ -0,0 +1,16 @@
+//
+//  WJSimpleNetworkTests-Bridge.h
+//  WJSimpleNetwork
+//
+//  Created by Kevin on 15/3/23.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+///  导入此框架才能引用MD5加密
+#import <CommonCrypto/CommonCrypto.h>
+
+#ifndef WJSimpleNetwork_WJSimpleNetwork_Bridge_h
+#define WJSimpleNetwork_WJSimpleNetwork_Bridge_h
+
+
+#endif
